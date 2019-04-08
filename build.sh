af8ee974efe1b8a7df3d144d3208514ce4595b28
James Gregory <james.jrg@gmail.com>
2019-04-08 12:08:08 +0100

    docker tag -f no longer exists

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ VERSION=$(cat conda-version)
 IMAGE=docker-registry2.elevate.internal:5000/elevate-python
 VERSIONED_IMAGE=${IMAGE}:${VERSION}
 docker build -t $IMAGE .
-docker tag -f $IMAGE $VERSIONED_IMAGE
+docker tag  $IMAGE $VERSIONED_IMAGE
 docker push $VERSIONED_IMAGE
 docker push $IMAGE
